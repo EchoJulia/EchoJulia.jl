fbe3ae129627f89cd72919e1f2c4752e91041dad
Rob Blackwell <rob@robblackwell.com>
2018-07-18 06:24:34 +0100

    loadraw

--- examples/raw2mat.jl
+++ examples/raw2mat.jl
@@ -8,11 +8,10 @@ using MAT
 
 function main(args)
 
-    transducers= []
-   
+    ecs_filename = nothing
+    
     if args[1] == "-c" # Echoview calibration supplement
-        filename = args[2]
-        transducers = load(filename)
+        ecs_filename = args[2]
         filenames = args[3:end]
     else
         filenames = args
@@ -37,98 +36,7 @@ function main(args)
 
         try
             info("Processing $filename ...")
-            ps = collect(pings(filename))
-
-            frequencies = unique([p.frequency for p in ps])
-            
-            dict = Dict()
-
-            index = 1
-            for f in frequencies
-                fr = trunc(Int,f / 1000)
-                psf = [p for p in ps if p.frequency == f]
-
-                gain = nothing
-                equivalentbeamangle = nothing
-                soundvelocity =  nothing
-                absorptioncoefficient = nothing
-                transmitpower = nothing
-                pulselength = nothing
-                sacorrection = nothing
-
-                if length(transducers) >= index
-                    transducer = transducers[index]
-                    gain = get(transducer, "Ek60TransducerGain", nothing)
-                    equivalentbeamangle = get(transducer,"TwoWayBeamAngle", nothing)
-                    soundvelocity=  get(transducer,"SoundSpeed", nothing)
-                    absorptioncoefficient= get(transducer,"AbsorptionCoefficient", nothing)
-                    transmitpower= get(transducer,"TransmittedPower", nothing)
-                    pulselength= get(transducer,"TransmittedPulseLength", nothing)
-                    sacorrection= get(transducer,"EK60SaCorrection", nothing)
-                    if pulselength != nothing
-                        pulselength = pulselength / 1000
-                    end
-                end
-                
-
-                dict["Sv$fr"] = SimradEK60.Sv(psf,
-                                              gain=gain,
-                                              equivalentbeamangle=equivalentbeamangle,
-                                              soundvelocity=soundvelocity,
-                                              absorptioncoefficient=absorptioncoefficient,
-                                              transmitpower=transmitpower,
-                                              pulselength=pulselength,
-                                              sacorrection=sacorrection);
-
-
-                dict["ntheta$fr"] = alongshipangle(psf)
-                dict["nphi$fr"] = athwartshipangle(psf)
-                dict["r$fr"] = R(psf, soundvelocity=soundvelocity)
-                dict["t$fr"] = SimradEK60.filetime(psf) # timestamps
-
-                if gain != nothing
-                    dict["gain$fr"] = gain
-                else
-                    dict["gain$fr"] = [x.gain for x in psf]
-                end
-                if equivalentbeamangle != nothing
-                    dict["equivalentbeamangle$fr"]= equivalentbeamangle
-                else
-                    dict["equivalentbeamangle$fr"]= [x.equivalentbeamangle for x in psf]
-                end
-                if soundvelocity != nothing
-                    dict["soundvelocity$fr"]= soundvelocity
-                else
-                    dict["soundvelocity$fr"]=  [x.soundvelocity for x in psf]
-                end
-                if absorptioncoefficient != nothing
-                    dict["absorptioncoefficient$fr"] = absorptioncoefficient
-                else
-                    dict["absorptioncoefficient$fr"] = [x.absorptioncoefficient for x in psf]
-                end
-                if transmitpower != nothing
-                    dict["transmitpower$fr"] = transmitpower
-                else
-                    dict["transmitpower$fr"] = [x.transmitpower for x in psf]
-                end
-                if pulselength != nothing
-                    dict["pulselength$fr"] = pulselength
-                else
-                    dict["pulselength$fr"] = [x.pulselength for x in psf]
-                end
-                if sacorrection != nothing
-                    dict["sacorrection$fr"] = sacorrection
-                else
-                     dict["sacorrection$fr"] =[x.sacorrection for x in psf]
-                end
-
-                dict["sampleinterval$fr"] =[x.sampleinterval for x in psf]
-                
-                index +=1
-            end
-
-            dict["DESCR"] = "Converted from $filename by EchoJulia."
-
+            dict = loadraw(filename, ecs_filename)
         
             info("Writing $out ...")
             matwrite(out, dict)
--- src/EchoJulia.jl
+++ src/EchoJulia.jl
@@ -2,6 +2,8 @@ module EchoJulia
 
 using Reexport
 
+export loadraw
+
 @reexport using SimradRaw
 @reexport using SimradEK60
 @reexport using EchogramPyPlot
@@ -9,5 +11,104 @@ using Reexport
 @reexport using EchoviewEvr
 @reexport using EchoviewEcs
 @reexport using SimradEK60TestData
+
+function loadraw(raw_filenames, ecs_filename=nothing)
+    transducers= []
+    if ecs_filename != nothing
+        transducers = load(ecs_filename)
+    end
+    
+    ps = collect(pings(raw_filenames))
+    frequencies = unique([p.frequency for p in ps])
+            
+    dict = Dict()
+
+    index = 1
+    for f in frequencies
+        fr = trunc(Int,f / 1000)
+        psf = [p for p in ps if p.frequency == f]
+
+        gain = nothing
+        equivalentbeamangle = nothing
+        soundvelocity =  nothing
+        absorptioncoefficient = nothing
+        transmitpower = nothing
+        pulselength = nothing
+        sacorrection = nothing
+
+        if length(transducers) >= index
+            transducer = transducers[index]
+            gain = get(transducer, "Ek60TransducerGain", nothing)
+            equivalentbeamangle = get(transducer,"TwoWayBeamAngle", nothing)
+            soundvelocity=  get(transducer,"SoundSpeed", nothing)
+            absorptioncoefficient= get(transducer,"AbsorptionCoefficient", nothing)
+            transmitpower= get(transducer,"TransmittedPower", nothing)
+            pulselength= get(transducer,"TransmittedPulseLength", nothing)
+            sacorrection= get(transducer,"EK60SaCorrection", nothing)
+            if pulselength != nothing
+                pulselength = pulselength / 1000
+            end
+        end
+
+        dict["Sv$fr"] = SimradEK60.Sv(psf,
+                                      gain=gain,
+                                      equivalentbeamangle=equivalentbeamangle,
+                                      soundvelocity=soundvelocity,
+                                      absorptioncoefficient=absorptioncoefficient,
+                                      transmitpower=transmitpower,
+                                      pulselength=pulselength,
+                                      sacorrection=sacorrection);
+
+
+        dict["ntheta$fr"] = alongshipangle(psf)
+        dict["nphi$fr"] = athwartshipangle(psf)
+        dict["r$fr"] = R(psf, soundvelocity=soundvelocity)
+        dict["t$fr"] = SimradEK60.filetime(psf) # timestamps
+
+        if gain != nothing
+            dict["gain$fr"] = gain
+        else
+            dict["gain$fr"] = [x.gain for x in psf]
+        end
+        if equivalentbeamangle != nothing
+            dict["equivalentbeamangle$fr"]= equivalentbeamangle
+        else
+            dict["equivalentbeamangle$fr"]= [x.equivalentbeamangle for x in psf]
+        end
+        if soundvelocity != nothing
+            dict["soundvelocity$fr"]= soundvelocity
+        else
+            dict["soundvelocity$fr"]=  [x.soundvelocity for x in psf]
+        end
+        if absorptioncoefficient != nothing
+            dict["absorptioncoefficient$fr"] = absorptioncoefficient
+        else
+            dict["absorptioncoefficient$fr"] = [x.absorptioncoefficient for x in psf]
+        end
+        if transmitpower != nothing
+            dict["transmitpower$fr"] = transmitpower
+        else
+            dict["transmitpower$fr"] = [x.transmitpower for x in psf]
+        end
+        if pulselength != nothing
+            dict["pulselength$fr"] = pulselength
+        else
+            dict["pulselength$fr"] = [x.pulselength for x in psf]
+        end
+        if sacorrection != nothing
+            dict["sacorrection$fr"] = sacorrection
+        else
+            dict["sacorrection$fr"] =[x.sacorrection for x in psf]
+        end
+
+        dict["sampleinterval$fr"] =[x.sampleinterval for x in psf]
+        
+        index +=1
+    end
+
+    dict["DESCR"] = "Converted by EchoJulia."
+
+    return dict
+end
 
 end # module
